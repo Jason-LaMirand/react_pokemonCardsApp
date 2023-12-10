b8d33a79425085590c331e2d1101dff9d4c00038
Jason-LaMirand <jaylamirand@gmail.com>
2023-12-10 14:24:07 -0600

    added project files to git repository

---
+++ src/helpers.js
@@ -0,0 +1,28 @@
+/* Select a random element from values array. */
+function choice(values) {
+  const randIdx = Math.floor(Math.random() * values.length);
+  return values[randIdx];
+}
+
+function formatPokemon(data) {
+  return {
+    id: uuid(),
+    front: data.sprites.front_default,
+    back: data.sprites.back_default,
+    name: data.name,
+    stats: data.stats.map(stat => ({
+      value: stat.base_stat,
+      name: stat.stat.name
+    }))
+  };
+}
+
+function formatCard(data) {
+  return {
+    image: data.cards[0].image,
+    id: uuid()
+  };
+}
+
+export { choice, formatPokemon, formatCard };
+
